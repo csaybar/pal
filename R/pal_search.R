e28eebe800f2eb8fd0caebaaf5a49656d0c722b5
Cesar Luis Aybar Camacho <aybar1994@gmail.com>
2018-12-16 22:45:47 -0500

    :fire: pal_search update
    Attributes:
     - tags:
     - colors:
     - n:
     - catalogs:

--- R/pal_search.R
+++ R/pal_search.R
@@ -1,8 +1,28 @@
-#' Search a palette
-pal_search <- function(name = 'viridis',
-                       comments = TRUE,
-                       catalogs = c('R','hex-color','cpt-city',
-                                    'colorBrewer','python'),
-                       plot = TRUE) {
-  
+#' Search a colour palettes by attributes
+#'
+#' Search you ideal palette using the pal's color collection. It is generated 
+#' from the website \href(cpt-city){http://soliton.vm.bytemark.co.uk/pub/cpt-city/},
+#' \href(hex-color){https://www.color-hex.com/} and also some R palletes libraries. 
+#' 
+#' You can search palettes using tags, a favorite color, a specific number of colors on 
+#' the palette, transparency, and if it presents uniform
+#' distribution.
+#' 
+#' @param name tags;
+#' @param name colors; 
+#' @param name n; 
+#' @param name catalogs; 
+#' @return Character. An unique 'key' value.
+#' @export
+pal_search <- function(tags = 'viridis',
+                       colors = 'ALL',
+                       n = Inf,
+                       catalogs = c('R','hex-color','cpt-city')) {
+  cptcity_db <- sysdata[["cptcity"]]
+  regex_exp <- '(?=.*%s)'
+  regex_expresion <- sprintf(regex_exp,tags) %>% 
+    paste(collapse = '')
+  search_color <- grepl(regex_expresion,cptcity_db$tags,perl = T) %>% 
+    which()
+  cptcity_db$tags[search_color]
 }
