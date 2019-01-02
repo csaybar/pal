a32ca07e77ab9207d630837de83c08e2fd2d4def
Cesar Luis Aybar Camacho <aybar1994@gmail.com>
2019-01-02 12:28:45 -0500

    visualization functions
    - pal_plot: Display multiple palettes in a plot
    - theme_pal:  A ggplot2 theme for plot multiple color palletes.

---
+++ R/theme_pal.R
@@ -0,0 +1,14 @@
+#' theme_pal
+#'
+#' A ggplot2 theme for plot multiple color palletes.
+#'
+#' @importFrom  ggplot2 theme_minimal theme
+#' @export
+theme_pal <- function() {
+  theme_minimal() %+replace% theme(legend.position = "none", axis.text.y = element_blank(),
+                                   axis.text.x = element_blank(), axis.ticks.x = element_blank(),
+                                   axis.ticks.y = element_blank(), axis.title.y = element_blank(),
+                                   axis.title.x = element_blank(), plot.title = element_text(hjust = 0.5),
+                                   axis.ticks.length = unit(0, "mm"), panel.grid.major = element_blank(),
+                                   panel.grid.minor = element_blank())
+}
