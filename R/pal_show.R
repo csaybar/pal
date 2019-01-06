e932cd374622f44a565b8f732f29d78d8e7f0c98
Cesar Luis Aybar Camacho <aybar1994@gmail.com>
2019-01-05 22:47:57 -0500

    pal_show update

--- R/pal_show.R
+++ R/pal_show.R
@@ -4,22 +4,12 @@ setMethod('show' , 'pal',
            }
 )
 
-pal_show <- function(x, ...) {
-  cat('class              :' , class(x), '\n')
-  ctl <- paste(x@global@catalogs,collapse = ', ')
+pal_show <- function(object, ...) {
+  cat('class              :' , class(object), '\n')
+  ctl <- paste(object@global@catalogs,collapse = ', ')
   cat('catalogs           :' , ctl, '\n')
-  cat('Range              :' , sprintf('%s to %s',x@n@min, x@n@max), '\n')
-  if (length(x@colors@colors) != 0)
-    cat('colors             :' , x@colors@colors, '\n')
-  if (length(x@tags@tags) != 0)
-    cat('Tags               :' , x@tags@tags, '\n')
-  cat('Number of palettes :' , nrow(x@db), '\n')
+  cat('Range              :' , sprintf('%s to %s',object@n@min, object@n@max), '\n')
+  cat('colors             :' , object@colors@colors, '\n')
+  cat('Tags               :' , object@tags@tags, '\n')
+  cat('Number of palettes :' , nrow(object@db), '\n')
 }
-
-
-
-bb <- pal_search() + pal_n(min = 150,max = 200)
-pal_search()  %>%  pal_n(min = 150,max = 200)
-bb@colors@colors <- 'cesar'
-bb@tags@tags <- 'cesar'
-raster()
